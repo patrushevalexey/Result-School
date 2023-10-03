386a279a7388436dbe9f317b29ec70386b33ac25
Alexey <patrushevalexey@ya.ru>
2023-10-03 03:04:25 +0300

    add Type convertion

--- 5. Work with String/task6/index.js
+++ 5. Work with String/task6/index.js
@@ -1,13 +1,9 @@
 const userText = prompt("Введите текст").trim();
-console.log(userText);
 
 const wordFromText = prompt("Введите слово из текста").trim();
-console.log(wordFromText);
 
 const indexOfWord = userText.indexOf(wordFromText);
-console.log(indexOfWord);
 
 const string = userText.slice(0, indexOfWord);
-console.log(string);
 
 alert(`Результат: ${string}`);
