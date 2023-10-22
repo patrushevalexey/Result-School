97dfa5056591c7b95de3c89ab33444f3164c906e
Alexey <patrushevalexey@ya.ru>
2023-10-22 03:03:11 +0300

    add registr check

--- 9. Arrays/task3/index.js
+++ 9. Arrays/task3/index.js
@@ -1,6 +1,12 @@
 const coffees = ['Latte', 'Cappuccino', 'Americano'];
+let coffeeName;
 
-let coffeeName = prompt().trim();
+const changeChoiseForList = () => {
+    coffeeName = prompt(`Поиск кофе по названию:`).trim().toLowerCase();
+    return coffeeName = coffeeName.charAt(0).toUpperCase() + coffeeName.slice(1);
+}
+
+coffeeName = changeChoiseForList();
 
 coffees.some((el) => { return el === coffeeName }) ?
     alert(`Держите ваш любимый кофе ${coffeeName}. Он ${coffees.findIndex((el) => { return el === coffeeName }) + 1} по популярности в нашей кофейне.`) :
